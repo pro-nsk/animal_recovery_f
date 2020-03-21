d6150084f43fdb4590533767c185243871b28c69
pro nsk mi <motors@live.ru>
2020-03-21 15:37:22 +0700

    fixes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,6 +6,7 @@ const Footer = () => (
         <span className="phone">
             <p><span className="fw500">+7 (913) 909 96-90 - Батуева Анастасия</span>, руководитель&nbsp;базы&nbsp;реабилитации</p>
             <p><span className="fw500">+7 (989) 321 49-57 - Полякова Екатерина</span>, организатор&nbsp;операционного&nbsp;дня</p>
+            <p><span className="fw500">+7 (913) 849 98-81 - Емельянова Екатерина</span>, сотрудничество&nbsp;и&nbsp;связи&nbsp;с&nbsp;общественностью</p>
         </span>
         <span className="links">
             <p className="email"><a href="mailto:animalrecovery54@gmail.com">animalrecovery54@gmail.com</a></p>
@@ -14,6 +15,7 @@ const Footer = () => (
                 <a className="fb" href="https://www.facebook.com/groups/265910850673111/" /><span className="fb-before"/>
                 <a className="vk" href="https://vk.com/animal_recovery54" /><span className="vk-before"/>
             </p>
+            <p className="email fs12"><a href="https://pro.nsk.ru">разработка сайта</a></p>
         </span>
     </div>
 )
